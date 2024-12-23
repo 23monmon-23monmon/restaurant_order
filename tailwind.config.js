37846ebadc48b9437f14acfc98381272e31b75e2
23monmon-23monmon <ayemyatmon10.dev@gmail.com>
2024-12-23 13:17:16 +0630

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "350px",
+      md: "400px",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
